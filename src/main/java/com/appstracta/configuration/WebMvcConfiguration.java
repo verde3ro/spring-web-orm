9dc5c36d54f6214bdbe556ee5d3d556a006ae4dd
verde3ro <verde3ro@gmail.com>
2022-05-21 11:51:44 -0500

    Se agreganconfiguraciones de spring

--- src/main/java/com/appstracta/configuration/WebMvcConfiguration.java
+++ src/main/java/com/appstracta/configuration/WebMvcConfiguration.java
@@ -12,13 +12,13 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.appstracta"})
-public class WebMvcConfiguration implements WebMvcConfigurer{
+public class WebMvcConfiguration implements WebMvcConfigurer {
 
 	@Bean
 	public InternalResourceViewResolver resolver() {
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setViewClass(JstlView.class);
-		resolver.setPrefix("/WEB-INF/views");
+		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 
 		return resolver;
